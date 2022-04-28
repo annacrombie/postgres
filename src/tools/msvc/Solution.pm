1b39e42a29b9fb484dcb858d9d1e05ca7ab22b14
Andres Freund <andres@anarazel.de>
2022-04-28 09:43:44 -0700

    fixup! meson: prereq: output and depencency tracking work.

--- src/tools/msvc/Solution.pm
+++ src/tools/msvc/Solution.pm
@@ -749,9 +749,8 @@ sub GenerateFiles
 			'src/backend/parser/gram.y'))
 	{
 		print "Generating preproc.y...\n";
-		chdir('src/interfaces/ecpg/preproc');
-		system('perl parse.pl < ../../../backend/parser/gram.y > preproc.y');
-		chdir('../../../..');
+		my $ecpg = 'src/interfaces/ecpg';
+		system("perl $ecpg/preproc/parse.pl $ecpg/preproc src/backend/parser/gram.y > $ecpg/preproc/preproc.y");
 	}
 
 	unless (-f "src/port/pg_config_paths.h")
